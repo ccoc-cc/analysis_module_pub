cebacfe38c1300320864be0699d7f928dfec7fdc
ccoc <jjl@j-jl.com>
2024-12-04 21:26:45 +0800

    rename; step

--- 01.主成分分析_PCA/0.src/ui.R
+++ 01.主成分分析_PCA/0.src/ui.R
@@ -4,11 +4,11 @@ ui <- fluidPage(
         sidebarPanel(
             fileInput("dataFile", "选择数据文件", accept = c(".txt", ".tsv")),
             fileInput("groupFile", "选择分组文件", accept = c(".txt", ".tsv")),
-            numericInput("plotHeight", "图片高度 (单位: in)", value = 6, min = 1),
-            numericInput("plotWidth", "图片宽度 (单位: in)", value = 6, min = 1),
+            numericInput("plotHeight", "图片高度 (单位: in)", value = 6, step = 0.1),
+            numericInput("plotWidth", "图片宽度 (单位: in)", value = 6, step = 0.1),
             textInput("filePrefix", "输出文件前缀", value = "out"),
             textInput("colorUser", "分组颜色", value = "default", placeholder = "#E64B35,#4DBBD5,#00A087,#3C5488"),
-            actionButton("run", "运行分析"),
+            actionButton("run", "运行"),
             downloadButton("downloadData", "下载结果")
         ),
         mainPanel(
